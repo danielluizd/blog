3dceb04094f381596cf53ce1be6dbbb51c17f99e
danielluizd <danielluizdourados@gmail.com>
2022-02-11 15:06:20 -0400

    Introdução aos botões para efeito de carrosel, por algum motivo código do JavaScript não está retornando erro, porém não funciona corretamente.

---
+++ script.js
@@ -0,0 +1,49 @@
+const numbers = [50, 100, 50];
+
+const sum = (x, y, z) => x + y + z;
+
+console.log(sum(numbers));
+
+const randomNumber = Math.round(Math.random() * 100);
+
+const obj = {
+  a: 1,
+  b: 2,
+};
+
+console.log(obj);
+
+const h = (w) => {
+  w.d = 3;
+};
+
+const q = (f) => {
+  h(f);
+};
+
+const i = (b) => {
+  q(b);
+};
+
+const v = { k: "t" };
+
+i(v);
+console.log(v);
+
+const timestamps = [
+  {
+    date: "3242348-9842340234",
+    value: 6,
+  },
+  {
+    date: "99e89-499958",
+    value: 31,
+  },
+  {
+    date: "8596646656666r488",
+    value: 17,
+  },
+];
+
+let accumulator = 0;
+const oddNumbers = [51, 97, 65, 23];
