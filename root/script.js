e647c806f04f90afaca459b1498e7bd03792674d
danielluizd <danielluizdourados@gmail.com>
2022-02-21 11:54:29 -0400

    Adição de cards para cada artigo, ao clicar em um o próximo abre.

--- root/script.js
+++ root/script.js
@@ -1,44 +1,14 @@
-// const slides = document.querySelectorAll('[data-js="carousel__item"]');
-// const nextButton = document.querySelector('[data-js="carousel__button--next"]');
-// const prevButton = document.querySelector('[data-js="carousel__button--prev"]');
+var accordionItem = document.getElementsByClassName("accordionItem");
+var accordionHD = document.getElementsByClassName("accordionItemTitle");
 
-// let currentSlideIndex = 0;
-
-// nextButton.addEventListener("click", () => {
-//   if (currentSlideIndex === slides.length - 1) {
-//     currentSlideIndex = 0;
-//   } else {
-//     currentSlideIndex++;
-//   }
-
-//   slides.forEach((slide) => {
-//     slide.classList.remove("carousel__item--visible");
-//   });
-//   slides[currentSlideIndex].classList.add("carousel__item--visible");
-// });
-
-// prevButton.addEventListener("click", () => {
-//   if (currentSlideIndex === 0) {
-//     currentSlideIndex = slides.length - 1;
-//   } else {
-//     currentSlideIndex--;
-//   }
-
-//   slides.forEach((slide) => {
-//     slide.classList.remove("carousel__item--visible");
-//   });
-//   slides[currentSlideIndex].classList.add("carousel__item--visible");
-// });
-
-var accItem = document.getElementsByClassName("accordionItem");
-var accHD = document.getElementsByClassName("accordionItemTitle");
-for (i = 0; i < accHD.length; i++) {
-  accHD[i].addEventListener("click", toggleItem, false);
+for (i = 0; i < accordionHD.length; i++) {
+  accordionHD[i].addEventListener("click", toggleItem, false);
 }
+
 function toggleItem() {
   var itemClass = this.parentNode.className;
-  for (i = 0; i < accItem.length; i++) {
-    accItem[i].className = "accordionItem hidden";
+  for (i = 0; i < accordionItem.length; i++) {
+    accordionItem[i].className = "accordionItem hidden";
   }
   if (itemClass == "accordionItem hidden") {
     this.parentNode.className = "accordionItem visible";
